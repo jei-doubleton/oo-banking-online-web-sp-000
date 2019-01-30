95e6ab2759d335b607433736212d8b3c332371a3
J. Singleton <jennifer.singleton@gmail.com>
2019-01-30 17:23:06 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,24 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    true if self.status = "open" && self.balance > 0 ! false
+
+  end
 
 end
